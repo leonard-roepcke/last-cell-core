f34afceaa3a6c32cd0063ebb4ce458d7819d3912
core-user <leonard.roepcke.2@gmail.com>
2025-07-17 16:27:14 +0200

    basic setup

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup(){
+    canvas = createCanvas(400, 400)
+    windowResized();
+}
+
+function draw(){
+    background(220)
+}
+
+function windowResized() {
+  let aspectRatio = 16 / 9;
+  let w = windowWidth;
+  let h = windowHeight;
+
+  if (w / h > aspectRatio) {
+    w = h * aspectRatio;
+  } else {
+    h = w / aspectRatio;
+  }
+
+  resizeCanvas(w, h);
+  centerCanvas();
+  
+}
+
+function centerCanvas() {
+  let x = (windowWidth - width) / 2;
+  let y = (windowHeight - height) / 2;
+  canvas.position(x, y);
+}
