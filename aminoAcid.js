3c02aa598505f4b5e8b23c665985791a972c7221
core-user <leonard.roepcke.2@gmail.com>
2025-07-18 10:46:24 +0200

    amino colection system

--- aminoAcid.js
+++ aminoAcid.js
@@ -1,11 +1,21 @@
-function addAminoAcid(drawHandler, pos=[0, 0], master){
+function addAminoAcid(drawHandler, pos=[0, 0], master, aminoHandler=false){
     //i can be used also from other classes not from AminoHandler but master class
     //neads to implement addSlave() that beckoms the objekt and add its to the list.
     pos[0]+=1;
     pos[1]+=1;
-    let slave = new Protein(drawHandler, proteinColors.navi, 0.3, master)
-    slave.pos.setPos(pos)
-    master.addSlave(slave)
+    
+    if (master == false){
+        let slave = new Protein(drawHandler, proteinColors.navi, 0.3, false)
+        slave.pos.setPos(pos)
+        aminoHandler.addSlave(slave)
+    }
+    else{
+        let slave = new Protein(drawHandler, proteinColors.navi, 0.3, master)
+        slave.pos.setPos(pos)
+        master.addSlave(slave)
+    }
+    
+    
 }
 
 class AminoHandler{
@@ -14,7 +24,7 @@ class AminoHandler{
         this.player = player;
         this.aminos = [];
         for (let i = 0; i < 20; i++) {
-            addAminoAcid(this.drawHandler, [randomInt(0, 100), randomInt(0, 50)], this)  
+            addAminoAcid(this.drawHandler, [randomInt(0, 100), randomInt(0, 50)], false, this)  
         }
     }
 
@@ -30,8 +40,9 @@ class AminoHandler{
                 let dy = playerPos[1] - aminoPos[1];
                 let distance = Math.sqrt(dx * dx + dy * dy);
 
-                if (distance < playerRad) {
+                if (distance < playerRad*2) {
                     this.player.addSlave(amino);
+                    amino.master = this.player
                 }
             }
         });
