cefb53f3e01da8becc1e70a166bf738bf7142335
core-user <leonard.roepcke.2@gmail.com>
2025-07-19 08:38:58 +0200

    spawning acids

--- aminoAcid.js
+++ aminoAcid.js
@@ -22,6 +22,7 @@ class AminoHandler{
     constructor(drawHandler, player){
         this.drawHandler = drawHandler;
         this.player = player;
+        this.aminoSpawrate = 0.07;
         this.aminos = [];
         for (let i = 0; i < 100; i++) {
             addAminoAcid(this.drawHandler, [randomInt(-200, 200), randomInt(-100, 100)], false, this)  
@@ -29,6 +30,7 @@ class AminoHandler{
     }
 
     update(){
+        this.addAminoAcids();
         for (let i = this.aminos.length - 1; i >= 0; i--) {
             let amino = this.aminos[i];
             amino.draw();
@@ -71,5 +73,48 @@ class AminoHandler{
 
     addSlave(amino){
         this.aminos.push(amino)
+    }
+
+
+    addAminoAcids() {
+        this.aminoSpawrate += 0.000000;
+        if (random() < this.aminoSpawrate) {
+            const playerPos = this.player.pos.getPos();
+
+            const viewWidth = 100;
+            const viewHeight = 50;
+
+            const minOffset = 50; 
+            const maxOffset = 150; 
+
+            let side = floor(random(4)); 
+
+            let offsetX = 0;
+            let offsetY = 0;
+
+            switch (side) {
+            case 0: 
+                offsetX = -random(minOffset, maxOffset);
+                offsetY = random(-maxOffset, maxOffset);
+                break;
+            case 1: 
+                offsetX = random(minOffset, maxOffset);
+                offsetY = random(-maxOffset, maxOffset);
+                break;
+            case 2: 
+                offsetX = random(-maxOffset, maxOffset);
+                offsetY = -random(minOffset, maxOffset);
+                break;
+            case 3: 
+                offsetX = random(-maxOffset, maxOffset);
+                offsetY = random(minOffset, maxOffset);
+                break;
+            }
+
+            const spawnX = playerPos[0] + offsetX;
+            const spawnY = playerPos[1] + offsetY;
+
+            addAminoAcid(this.drawHandler, [spawnX, spawnY], false, this);
+        }
     }
 }
